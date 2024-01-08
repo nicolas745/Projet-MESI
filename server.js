5de9887b9299a731d9299a8e912e6b450b18648b
LAJAVEL <melvynneyele69@gmail.com>
2024-01-08 16:14:56 +0100

    AJOUT DE REACT NPM NODEJS

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
+  res.write('salut');
   res.end();
 }).listen(80);

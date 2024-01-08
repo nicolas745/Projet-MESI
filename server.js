9d6438788f99e39c237c2c55ca50e96b3f05900c
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2024-01-08 15:54:42 +0100

    Correction bug express

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-var http = require('http');
-var express = require('express');
-var socketIO = require('socket.io');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
-
-// Set up Express routes
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + 'src/index.html');
 });
 
-// Set up Socket.io events
-io.on('connection', function (socket) {
-  console.log('A user connected');
-
-  socket.on('disconnect', function () {
-    console.log('User disconnected');
-  });
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
-// Set up HTTP server
-server.listen(80, function () {
-  console.log('Server is listening on port 80');
+server.listen(80, () => {
+  console.log('listening on *:80');
 });

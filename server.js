5f85b91b3d9799599165ada9a4d799c89d414663
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2024-01-08 15:33:21 +0100

    modification fichier serveur

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
-}).listen(80);
+var express = require('express');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+// Set up Express routes
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + 'src/index.html');
+});
+
+// Set up Socket.io events
+io.on('connection', function (socket) {
+  console.log('A user connected');
+
+  socket.on('disconnect', function () {
+    console.log('User disconnected');
+  });
+});
+
+// Set up HTTP server
+server.listen(80, function () {
+  console.log('Server is listening on port 80');
+});

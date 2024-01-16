16353b4b60d5492ff6f5c1e552623ccd83630326
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2024-01-16 19:39:51 +0100

    modification total du back-end

--- serv/index.js
+++ serv/index.js
@@ -1,14 +1,19 @@
 class Page {
   constructor(app) {
     let bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({extended : true}));
-    let mustacheExpress = require('mustache-express');
-    app.engine('mustache', mustacheExpress()); 
-    app.engine('mustache', mustacheExpress(__dirname + '/../views/partials', '.mustache'));
-    app.set('view engine', 'mustache');
-    app.set('views', __dirname + '/../views'); 
+    const fs = require('fs');
     app.get('*', (req, res) => {
-        res.render('index', { title: 'Bonjour', message: 'Bienvenue sur notre site!' });
+        let url = 'mesi2024/src'+req.url;
+        if (url.endsWith("/")) {
+            url = url + "index.html";
+        }
+        fs.readFile(url, 'utf-8', (erreur, contenuFichier) => {
+            if (erreur) {
+              console.error('Erreur lors de la lecture du fichier :', erreur.message);
+            } else {
+              res.end(contenuFichier);
+            }
+          });
     });
     app.post('*', (req, res) => {
         res.render('index', { title: 'Bonjour', message: 'Bienvenue sur notre site!' });
--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 let page = require("./serv/index")
 new page(app)
-app.get('*', (req, res) => {
-  res.render('index', { title: 'Bonjour', message: 'Bienvenue sur notre site!' });
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected');

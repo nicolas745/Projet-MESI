739eea96f7a6ba146119ff66981a603ae90641a1
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2024-01-17 23:20:37 +0100

    back-end react ok

--- serv/index.js
+++ serv/index.js
@@ -1,19 +1,9 @@
+
 class Page {
   constructor(app) {
-    let bodyParser = require('body-parser');
-    const fs = require('fs');
+    const path = require('path')
     app.get('*', (req, res) => {
-        let url = 'mesi2024/src'+req.url;
-        if (url.endsWith("/")) {
-            url = url + "index.html";
-        }
-        fs.readFile(url, 'utf-8', (erreur, contenuFichier) => {
-            if (erreur) {
-              console.error('Erreur lors de la lecture du fichier :', erreur.message);
-            } else {
-              res.end(contenuFichier);
-            }
-          });
+      res.sendFile(path.join(__dirname,"./mesi2024/build/index.html"))
     });
     app.post('*', (req, res) => {
         res.render('index', { title: 'Bonjour', message: 'Bienvenue sur notre site!' });
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 let page = require("./serv/index")
+app.use(express.static('mesi2024/build'))
 new page(app)
-
 io.on('connection', (socket) => {
   console.log('a user connected');
 });

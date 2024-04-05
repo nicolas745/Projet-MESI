bf4fe60729c110424e0c5bce6751333de773cc90
Yohann04 <95697982+Yohann04@users.noreply.github.com>
2024-04-05 11:50:37 +0200

    Initialisation de l'acceuil

---
+++ mesi2024/src/ActionButtons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ActionButtons.css';
+
+const ActionButtons = () => {
+  return (
+    <div className="action-buttons">
+      <button className="watch-button">Regarder le film</button>
+      <button className="info-button">Plus d'informations</button>
+    </div>
+  );
+}
+
+export default ActionButtons;

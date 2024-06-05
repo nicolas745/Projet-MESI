9e438fcf1d78c4b84cc99056eeeb74adcabbea55
nicolas <47307856+nicolas745@users.noreply.github.com>
2024-06-05 16:44:20 +0200

    gestion video

--- src/sql/video.py
+++ src/sql/video.py
@@ -4,7 +4,26 @@ class video:
         self.db = DB()
     def getinfo(self,id):
         self.db.open()
-        info=self.db.execute("SELECT `contenu_id`, `description`, `titre`, `video`, `image`, `Auteur.nom`, `Auteur.prenom` INNER JOIN Auteur ON contenu.auteur_id = Auteur.auteur_id  FROM contenu WHERE contenu_id=?;",[id])
+        info=self.db.execute("SELECT `contenu_id`, `description`, `titre`, `video`, `image`, `Auteur.nom`, `Auteur.prenom` INNER JOIN Auteur ON contenu.auteur_id = Auteur.auteur_id  FROM contenu WHERE contenu_id=?;",(id,))
         self.db.close()
         return info
-    
+
+    def list_videos(self, num_elements, page_number):
+        self.db.open()
+        offset = (page_number - 1) * num_elements
+        query = "SELECT `contenu_id`, `description`, `titre`, `video`, `image`, `Auteur.nom`, `Auteur.prenom` FROM contenu INNER JOIN Auteur ON contenu.auteur_id = Auteur.auteur_id LIMIT ? OFFSET ?;"
+        videos = self.db.execute(query, (num_elements, offset))
+        self.db.close()
+        return videos
+
+    def add_video(self,description, titre ,urlvideo,image,auteur_id ):
+        self.db.open()
+        query = "INSERT INTO contenu (`description`, `titre`, `video`, `image`, `auteur_id`) VALUES (? , ? , ? , ? , ?);"
+        self.db.execute(query, (description, titre ,urlvideo,image,auteur_id  ))
+        self.db.close()
+
+    def delete_video(self, id):
+        self.db.open()
+        query = "DELETE FROM contenu WHERE contenu_id = ?;"
+        self.db.execute(query, [id])
+        self.db.close()

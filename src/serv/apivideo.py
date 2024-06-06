1820216f7a266f832b2cccb9d89ea8f7eeab89f5
nicolas <47307856+nicolas745@users.noreply.github.com>
2024-06-06 10:28:19 +0200

    corection bug critique

--- src/serv/apivideo.py
+++ src/serv/apivideo.py
@@ -1,14 +1,14 @@
-from serv.serv import serv , url
-from sql.video import video
-from flask import Flask, request
-class apivideo(serv):
-    def __init__(self, app: Flask):
-        super().__init__(app)
-    @url("/api/video/list",['GET'])
-    def listvideo(self):
-        if('page' in request.args):
-            return video().list_videos(10,request.args.get('page'))
-        return video().list_videos(10,0)
-    @url("api/video", ['GET'])
-    def video(self):
-        return video().getinfo(request.args.get('id'))
+#from serv.serv import serv , url
+#from sql.video import video
+#from flask import Flask, request
+#class apivideo(serv):
+#    def __init__(self, app: Flask):
+#        super().__init__(app)
+#    @url("/api/video/list",['GET'])
+#    def listvideo(self):
+#        if('page' in request.args):
+#            return video().list_videos(10,request.args.get('page'))
+#        return video().list_videos(10,0)
+#    @url("api/video", ['GET'])
+#    def video(self):
+#        return video().getinfo(request.args.get('id'))
--- src/serv/pagevideo.py
+++ src/serv/pagevideo.py
@@ -1,12 +1,12 @@
-from serv.serv import serv , url
-from sql.video import video
-from flask import Flask, request
-class pagevideo(serv):
-    def __init__(self, app: Flask):
-        super().__init__(app)
-    @url("index.html", ['GET'])
-    def index(self):
-        args = {
-            'listvideo':video().list_videos(10,0)
-        }
-        return self.page("index.html",arg=args)
+#from serv.serv import serv , url
+#from sql.video import video
+#from flask import Flask, request
+#class pagevideo(serv):
+#    def __init__(self, app: Flask):
+#        super().__init__(app)
+#    @url("index.html", ['GET'])
+#    def index(self):
+#        args = {
+#            'listvideo':video().list_videos(10,0)
+#        }
+#        return self.page("index.html",arg=args)
